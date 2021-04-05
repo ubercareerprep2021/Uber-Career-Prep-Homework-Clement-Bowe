62c96aff637d73bb4777055de6c1da3033c6474b
Clement Bowe <cbowe023@myfiu.edu>
2021-04-04 22:41:06 -0400

    pairs that add to sum

--- Assignment1/part1/src/App.java
+++ Assignment1/part1/src/App.java
@@ -25,11 +25,46 @@ public class App {
         }
         return true;
     }
+
+    public static List<List<Integer>> pairsThatEqualSum(List<Integer> inputArray, Integer targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Collections.sort(inputArray);
+
+        if(inputArray.size() == 0 || inputArray == null){
+            return ans; 
+        }
+
+        Collections.sort(inputArray);
+        for(int i = 0; i < inputArray.size()-1; i++){
+            int element = inputArray.get(i);
+            int j = inputArray.size()-1;
+            while(j > i){
+                int sum = element+inputArray.get(j);
+                if(sum == targetSum){
+                    List<Integer> arr = new ArrayList<>();
+                    arr.add(element);
+                    arr.add(inputArray.get(j));
+                    ans.add(arr);
+                }
+                j--;
+            }
+        }
+
+        return ans;
+    }
     
     public static void main(String[] args) throws Exception {
+        Random rand = new Random();
         Scanner in = new Scanner(System.in);
        String word1 = in.next();
        String word2 = in.next();
-       System.out.println(isStringPermutation(word1,word2)); 
+       System.out.println(isStringPermutation(word1,word2));
+
+       List<Integer> let = new ArrayList<>();
+       for(int i = 0; i < 5; i++){
+           int el = in.nextInt();
+           let.add(el);
+       }
+      System.out.println(pairsThatEqualSum(let, 12));
     }
 }

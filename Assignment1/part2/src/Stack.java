f5f26c48dbf8ab43c5086858b3b59856c3dcff77
Clement Bowe <cbowe023@myfiu.edu>
2021-05-08 00:45:17 -0400

    modified stack code

--- Assignment1/part2/src/Stack.java
+++ Assignment1/part2/src/Stack.java
@@ -1,5 +1,4 @@
 public class Stack {
-    
 	    Node head;
         private int size;
         public Stack(){
@@ -22,9 +21,9 @@ public class Stack {
 
     public void push(Integer number) {
 			if(head == null){
-				head = new Node(number, number,number);
+				head = new Node(number, number);
 			} else{
-				head = new Node(number, Math.min(number, head.min), Math.max(number,head.max),head);
+				head = new Node(number, Math.max(number,head.max),head);
 			}
             size++;
     }
@@ -39,19 +38,16 @@ public class Stack {
       return head.max;
     }
     private class Node{
-        Integer node;
-        Integer min;
         Integer max;
         Integer val; 
         Node next;
-        private Node(Integer val, int min, int max, Node next){
+        private Node(Integer val, int max, Node next){
             this.val = val;
-            this.min = min;
             this.max = max;
             this.next = next;
         }
-        private Node(Integer val, Integer min, Integer max){
-            this(val,min,max,null);
+        private Node(Integer val, Integer max){
+            this(val, max, null);
         }
     }
 }
